72e001cbbeb8c254da4f57def696bb8724736839
kiddynamo <njenan@gmail.com>
2017-09-14 17:31:33 -0700

    Adding routing

--- myApp/App.js
+++ myApp/App.js
@@ -1,94 +1,45 @@
 import React from 'react';
-import styles from './style';
-import {Animated, Platform, Button, Text, Image, View} from 'react-native';
+import {
+    AppRegistry,
+    Button,
+    Text,
+    View
+} from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
 
-export default class App extends React.Component {
-    state = {
-        fade: new Animated.Value(0),
-        left: new Animated.Value(-200),
-        spin: new Animated.Value(0)
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
     };
 
     render() {
-        let platformSpecificText = Platform.select({
-            ios: "Go to App Store",
-            android: "Go to Play Store"
-        });
+        let { navigate } = this.props.navigation;
+        return <View>
+            
+            <Text>Hello, Navigation!</Text>
+            <Button title="Go To List" onPress={() => navigate('List')}></Button>
 
-        let fade = this.state.fade;
-        let left = this.state.left;
-
-        let spin = this.state.spin.interpolate({
-            inputRange: [0, 1],
-            outputRange: ['0deg', '360deg']
-        });
-
-        return (
-            <View style={styles.container}>
-                <Text>Hello {Platform.OS} users!</Text>
-                <Button title={platformSpecificText} onPress={function () {
-                }}>Asdf</Button>
-
-                <Animated.View style={{opacity: fade}}>
-                    <Text>Fading in</Text>
-                </Animated.View>
-                <Animated.View style={{marginLeft: left}}>
-                    <Text>Bouncing</Text>
-                </Animated.View>
-                <Animated.Image style={{transform: [{rotate: spin}]}} source={require("./smiley.jpg")}/>
-            </View>
-        );
-    }
-
-    componentDidMount() {
-        Animated.timing(
-            this.state.fade,
-            {
-                toValue: 1, duration: 5000
-            })
-            .start();
-
-        let left = this.state.left;
-
-        function bounceLeft() {
-            Animated.spring(
-                left,
-                {
-                    toValue: 200
-                })
-                .start(bounceRight);
-        }
-
-        function bounceRight() {
-            Animated.spring(
-                left,
-                {
-                    toValue: -200
-                })
-                .start(bounceLeft);
-        }
-
-        bounceLeft();
-
-        let spin = this.state.spin;
-
-        function spinner() {
-            Animated.sequence([
-                Animated.timing(
-                    spin,
-                    {
-                        toValue: 1
-                    }),
-                Animated.timing(
-                    spin,
-                    {
-                        toValue: 0
-                    })
-            ])
-                .start(spinner);
-        }
-
-        spinner();
+        </View>;
     }
 }
+
+class List extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+
+    render() {
+        return <View>
+            <Text>Here is your list!</Text>
+            
+
+        </View>;
+    }
+}
+
+export default SimpleApp = StackNavigator({
+    Home: {screen: HomeScreen},
+    List: {screen: List}
+});
+
